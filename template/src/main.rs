6b1852c09e98a95243bded5560cc645f21bbbf6c
momobel <plopyomomo@gmail.com>
2020-12-10 23:37:14 +0100

    Add day template

---
+++ template/src/main.rs
@@ -0,0 +1,32 @@
+use std::{env, fs};
+
+fn get_input_path() -> String {
+    let args: Vec<String> = env::args().collect();
+    args.get(1).unwrap().clone()
+}
+
+type Input = ();
+type Output1 = ();
+type Output2 = ();
+
+fn parse_input(input: &str) -> Input {
+    unimplemented!()
+}
+
+fn solve_part_1(input: &Input) -> Output1 {
+    unimplemented!()
+}
+
+fn solve_part_2(input: &Input) -> Output2 {
+    unimplemented!()
+}
+
+fn main() {
+    let input_path = get_input_path();
+    let raw_input = fs::read_to_string(input_path).unwrap();
+    let input = parse_input(&raw_input);
+    let part_1_result = solve_part_1(&input);
+    println!("Part 1: {:?}", part_1_result);
+    let part_2_result = solve_part_2(&input);
+    println!("Part 2: {:?}", part_2_result);
+}
