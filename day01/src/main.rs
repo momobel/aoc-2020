d16619299db564f41f182745feebaa74e38502b1
momobel <plopyomomo@gmail.com>
2020-12-10 21:57:25 +0100

    Add day 1 part 1

---
+++ day01/src/main.rs
@@ -0,0 +1,33 @@
+use std::{env, fs};
+
+fn get_input_path() -> String {
+    let args: Vec<String> = env::args().collect();
+    args.get(1).unwrap().clone()
+}
+
+type Input = Vec<u32>;
+
+fn parse_input(input: &str) -> Input {
+    input.lines().map(|l| l.parse::<u32>().unwrap()).collect()
+}
+
+fn part_1(input: &Input) -> u32 {
+    let mut first = input.iter();
+    while let Some(a) = first.next() {
+        let mut second = first.clone();
+        while let Some(b) = second.next() {
+            if a + b == 2020 {
+                return a * b;
+            }
+        }
+    }
+    panic!("Solution not found");
+}
+
+fn main() {
+    let input_path = get_input_path();
+    let raw_input = fs::read_to_string(input_path).unwrap();
+    let input = parse_input(&raw_input);
+    let part_1_result = part_1(&input);
+    println!("Part 1: {}", part_1_result);
+}
