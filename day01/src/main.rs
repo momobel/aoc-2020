33354f890d59d29d45dd4d66fa4a581ab9d7a175
momobel <plopyomomo@gmail.com>
2020-12-10 22:12:31 +0100

    Day 01 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -24,10 +24,28 @@ fn part_1(input: &Input) -> u32 {
     panic!("Solution not found");
 }
 
+fn part_2(input: &Input) -> u32 {
+    let mut first = input.iter();
+    while let Some(a) = first.next() {
+        let mut second = first.clone();
+        while let Some(b) = second.next() {
+            let mut third = second.clone();
+            while let Some(c) = third.next() {
+                if a + b + c == 2020 {
+                    return a * b * c;
+                }
+            }
+        }
+    }
+    panic!("Solution not found");
+}
+
 fn main() {
     let input_path = get_input_path();
     let raw_input = fs::read_to_string(input_path).unwrap();
     let input = parse_input(&raw_input);
     let part_1_result = part_1(&input);
     println!("Part 1: {}", part_1_result);
+    let part_2_result = part_2(&input);
+    println!("Part 2: {}", part_2_result);
 }
