ec6c86e7f18b10f3c4764a65df718d9f42b14495
momobel <plopyomomo@gmail.com>
2020-12-13 02:00:59 +0100

    day08 part 2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,16 +1,18 @@
-use std::{env, fs};
+use std::{collections::HashSet, env, fs};
 
 fn get_input_path() -> String {
     let args: Vec<String> = env::args().collect();
     args.get(1).unwrap().clone()
 }
 
+#[derive(Clone, Copy)]
 enum Operation {
     Noop,
     Acc,
     Jump,
 }
 
+#[derive(Clone, Copy)]
 struct Instruction {
     pub op: Operation,
     pub arg: i32,
@@ -19,7 +21,7 @@ struct Instruction {
 type Program = Vec<Instruction>;
 type Input = Program;
 type Output1 = i32;
-type Output2 = ();
+type Output2 = i32;
 
 fn parse_input(input: &str) -> Input {
     input
@@ -45,6 +47,7 @@ struct Machine<'a> {
     pub iptr: usize,
 }
 
+#[derive(PartialEq)]
 enum MachineState {
     Running,
     Terminated,
@@ -74,6 +77,17 @@ impl<'a> Machine<'a> {
         }
         MachineState::Running
     }
+
+    pub fn state(&self) -> MachineState {
+        match self.prog.get(self.iptr) {
+            Some(_) => MachineState::Running,
+            None => MachineState::Terminated,
+        }
+    }
+
+    pub fn next_op(&self) -> Operation {
+        self.prog.get(self.iptr).unwrap().op
+    }
 }
 
 fn solve_part_1(input: &Input) -> Output1 {
@@ -89,8 +103,50 @@ fn solve_part_1(input: &Input) -> Output1 {
     m.acc
 }
 
+fn run_best_effort(m: &mut Machine, exec_cache: &mut [bool]) -> HashSet<usize> {
+    exec_cache.iter_mut().for_each(|x| *x = false);
+    let mut flow_ctrl_visited: HashSet<usize> = HashSet::new();
+    while m.state() == MachineState::Running && !exec_cache[m.iptr] {
+        exec_cache[m.iptr] = true;
+        match m.next_op() {
+            Operation::Jump | Operation::Noop => {
+                flow_ctrl_visited.insert(m.iptr);
+            }
+            _ => {}
+        }
+        m.step();
+    }
+    flow_ctrl_visited
+}
+
 fn solve_part_2(input: &Input) -> Output2 {
-    unimplemented!()
+    let mut executed: Vec<bool> = Vec::with_capacity(input.len());
+    for _ in 0..input.len() {
+        executed.push(false);
+    }
+    let flow_ctrls = run_best_effort(&mut Machine::new(input), &mut executed);
+    for to_mod in flow_ctrls {
+        let mut modded = input.clone();
+        let patch = modded.get_mut(to_mod).unwrap();
+        match patch {
+            Instruction {
+                op: Operation::Noop,
+                ..
+            } => patch.op = Operation::Jump,
+            Instruction {
+                op: Operation::Jump,
+                ..
+            } => patch.op = Operation::Noop,
+            _ => panic!("Acc instruction shall not be patched"),
+        };
+        let mut m: Machine = Machine::new(&modded);
+        let flow_ctrl_execed = run_best_effort(&mut m, &mut executed);
+        match m.state() {
+            MachineState::Terminated => return m.acc,
+            MachineState::Running => continue,
+        }
+    }
+    panic!("Didn't find a proper instruction to patch");
 }
 
 fn main() {
@@ -121,5 +177,12 @@ acc +6";
         let input = parse_input(EX_INPUT);
         let res = solve_part_1(&input);
         assert_eq!(5, res);
+    }
+
+    #[test]
+    fn test_part2_ex() {
+        let input = parse_input(EX_INPUT);
+        let res = solve_part_2(&input);
+        assert_eq!(8, res);
     }
 }
